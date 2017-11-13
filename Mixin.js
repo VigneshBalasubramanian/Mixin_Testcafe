032b0998daf0ccf315aabe7109f25ef8f0fba43a
Vignesh Balasubramanian <vigneshb.connect@gmail.com>
2017-11-13 09:55:10 -0600

    Mixins

--- Mixin.js
+++ Mixin.js
@@ -1,35 +1,36 @@
-/*	Testcafe for authorization, Mixins..
-	v 1.0
-	Dated 11/8/2017	
-*/
-
 import { Selector } from 'testcafe';
-
-'@require ./mixin.js';
 
 fixture `Mixins`
 	.page `https://www.python.org/accounts/login/`;
-
-
-        var input = $('#id_login');
-        act.type(input, 'vigneshb.connect@gmail.com');
-
-        var input = $('#id_password');
-        act.type(input, '987654');
-
-        var submitButton = $('#submit');
-        act.click(submitButton);
-  
- 
-test('Profile', async t => {
-
- 
-        var link = $(':containsExcludeChildren(View your public profile)');
-        await t.expect(act.click(link)).ok();
 	
+test('Mixin_Login', async t => {
+	
+	
+    await t
+        .typeText(Selector('input').withAttribute('placeholder', 'Username'), "vigneshb.connect@gmail.com") 			//Email entry//
+        .typeText(Selector('input').withAttribute('placeholder', 'Password'), "987654")			
+					//Password entry//
+		.click(Selector('button').withAttribute('class', 'primaryAction'));
+					//Submit//
+	await mixin_Search(t, "Django");										//Search//
+	await mixin_Navigation(t, "psf-meta ");									//Navigation//
+	await mixin_Search(t, "integration");
+	await mixin_Navigation(t, "shop-meta ");
+	await mixin_Navigation(t, "jobs-meta ");
+	await t.click(Selector('h1').withAttribute('class', 'site-headline')); 	//Home page //
 
-
-        var link = $(':containsExcludeChildren(Edit profile)');
-        await t.expect(act.click(link)).ok();
-    
 });
+
+//Search//
+const mixin_Search = async (t, searchString) => {
+			
+	await t
+		.typeText(Selector('input').withAttribute('placeholder', 'Search'), searchString)			
+					//Password entry//
+		.click(Selector('button').withAttribute('class', 'search-button'))
+}
+
+//Page Navigation//
+const mixin_Navigation = async (t, page_link) => {
+	await t.click(Selector('li').withAttribute('class', page_link));
+}
